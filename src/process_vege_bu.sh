f21676f631250395ba9b4703af2232b7f96ce7eb
jgaffuri <jgaf@proton.me>
2024-11-24 15:28:07 +0100

    a

--- src/process_vege_bu.sh
+++ src/process_vege_bu.sh
@@ -1,6 +1,13 @@
 
+echo "pipeline vege"
 pdal pipeline p_veg.json
+
+echo "color vege"
 gdaldem color-relief "../tmp/vegetation.tif" -of GTiff -alpha color_table_vege.txt "../tmp/vegetation_color.tif"
 
-pdal pipeline p_veg.json
+
+echo "pipeline bu"
+pdal pipeline p_bu.json
+
+echo "color bu"
 gdaldem color-relief "../tmp/building.tif" -of GTiff -alpha color_table_bu.txt "../tmp/building_color.tif"
