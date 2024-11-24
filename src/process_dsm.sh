d28a9c650c9f983b2251dbbde3840c5b058b4e75
jgaffuri <jgaf@proton.me>
2024-11-24 12:09:05 +0100

    a

---
+++ src/process_dsm.sh
@@ -0,0 +1,11 @@
+#conda activate myenv
+
+echo "pipeline" 
+pdal pipeline p.json
+
+echo "hillshading" 
+gdaldem hillshade ../tmp/dsm.tif ../tmp/hillshade.tif -z 1 -s 1 -az 315 -alt 45
+
+echo "slope"
+gdaldem slope ../tmp/dsm.tif ../tmp/slope.tif -s 1
+
