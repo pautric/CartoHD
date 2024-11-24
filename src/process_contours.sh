40bb9da4758b055c3ef48ca390ef2891aa38eb9f
jgaffuri <jgaf@proton.me>
2024-11-24 16:12:29 +0100

    a

--- src/process_contours.sh
+++ src/process_contours.sh
@@ -1,4 +1,9 @@
 
-echo "Generalise DTM 1m"
+echo "Generalise DTM"
+
+#use smoothing instead
 gdalwarp -tr 1 1 -r bilinear ../tmp/dtm.tif ../tmp/dtm_g1.tif
+
+echo "Make contours"
 gdal_contour -a elevation -i 5 -f GPKG ../tmp/dtm_g1.tif ../tmp/contours_g1.gpkg
+
