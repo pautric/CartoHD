86a31302e20a87d0eb19a4a174ffbe18973e59e6
jgaffuri <jgaf@proton.me>
2024-11-27 19:48:14 +0100

    a

--- src/process_dsm.sh
+++ src/process_dsm.sh
@@ -3,7 +3,8 @@
 echo "pipeline DSM" 
 pdal pipeline p_dsm.json
 
-gdal_fillnodata.py -md 20 -of GTiff dsm_raw.tif dsm.tif
+echo "fill no data" 
+gdal_fillnodata.py -md 20 -of GTiff ../tmp/dsm_raw.tif ../tmp/dsm.tif
 
 echo "hillshading" 
 gdaldem hillshade ../tmp/dsm.tif ../tmp/hillshade.tif -z 1 -s 1 -az 315 -alt 45
