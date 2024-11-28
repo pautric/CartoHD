48fe75d6d740a686493e9834e1ba9c122449bd6e
jgaffuri <jgaf@proton.me>
2024-11-29 00:23:22 +0100

    a

---
+++ src/old/process.py
@@ -0,0 +1,38 @@
+import pdal
+import json
+
+
+tiles = "/home/juju/Bureau/lidar_test/tiles/*.laz"
+outfolder = "../tmp/"
+
+
+# Define the PDAL pipeline as a JSON string
+pipeline = [
+    {
+        "type": "readers.las",
+        "filename": tiles
+    },
+    {
+        "limits": "Classification![7:7]",
+        "type": "filters.range",
+        "tag": "nonoise"
+    },
+    {
+        "type": "filters.ferry",
+        "dimensions": "Z=>elevation"
+    },
+    {
+        "type": "writers.gdal",
+        "filename": outfolder + "dsm_raw.tif",
+        "resolution": 0.2,
+        "output_type": "max"
+    }
+]
+
+pipeline_json = json.dumps(pipeline)
+pipeline_obj = pdal.Pipeline(pipeline_json)
+pipeline_obj.execute()
+
+log = pipeline_obj.log
+print(log)
+
--- src/process.py
+++ src/process.py
@@ -1,38 +1,10 @@
-import pdal
-import json
+import subprocess
 
 
-tiles = "/home/juju/Bureau/lidar_test/tiles/*.laz"
-outfolder = "../tmp/"
 
-
-# Define the PDAL pipeline as a JSON string
-pipeline = [
-    {
-        "type": "readers.las",
-        "filename": tiles
-    },
-    {
-        "limits": "Classification![7:7]",
-        "type": "filters.range",
-        "tag": "nonoise"
-    },
-    {
-        "type": "filters.ferry",
-        "dimensions": "Z=>elevation"
-    },
-    {
-        "type": "writers.gdal",
-        "filename": outfolder + "dsm_raw.tif",
-        "resolution": 0.2,
-        "output_type": "max"
-    }
-]
-
-pipeline_json = json.dumps(pipeline)
-pipeline_obj = pdal.Pipeline(pipeline_json)
-pipeline_obj.execute()
-
-log = pipeline_obj.log
-print(log)
+command = ["pdal", "pipeline", "src/p_dsm.json"]
+result = subprocess.run(command, capture_output=True, text=True)
+print(result.stdout)
+if result.stderr:
+    print("Error:", result.stderr)
 
