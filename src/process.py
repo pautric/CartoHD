21161738b2a5c4cceb3e6cf31a421bca0b37d8c9
jgaffuri <jgaf@proton.me>
2025-01-03 21:07:05 +0100

    a

--- src/process.py
+++ src/process.py
@@ -12,7 +12,6 @@ idf = "/home/juju/geodata/lidar/"
 for f in ["aix_en_provence", "toulon", "crest",
           "guillestre", "lyon", "hk", "banyuls", "caen", "bordeaux", "grenoble",
           "la_rochelle", "lezinnes", "nice", "reims", "rouen", "bourges"]:
-    # "toulouse", "cirque_gavarnie", "villandry", "ile_aix", "larzac", "mont_blanc", "dune_pilat"
     print(f)
 
 
