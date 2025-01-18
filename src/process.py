d8a294e07c73c516446ee5cc41fb28da8a19f2a9
jgaffuri <jgaf@proton.me>
2025-01-18 21:34:27 +0100

    a

--- src/process.py
+++ src/process.py
@@ -25,13 +25,13 @@ import os
 
 
 idf = "/home/juju/geodata/lidar/"
-for f in [ "le_mans", "auxerre", "avignon", "valence", "mulhouse", "tours", "carcassonne" ]:
+for f in ["mulhouse", "tours", "carcassonne", "marseille" ]:
     print(f)
 
     print("download")
     os.makedirs(idf+f+"/", exist_ok=True)
     #if f != "carcassonne":
-    run_command(["wget", "-i", idf+f+".txt", "-P", idf+f+"/"])
+    run_command(["wget", "-i", "-nc", idf+f+".txt", "-P", idf+f+"/"])
 
     input_lidar_data = idf+f+"/*.laz"
     output_folder = "/home/juju/lidar_mapping/"+f+"/"
